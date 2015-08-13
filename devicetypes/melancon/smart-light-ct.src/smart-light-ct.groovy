19986badd5d73c9350e1b5900592b572eaf5e4e0
Michael Melancon <mike@themelancons.net>
2015-08-13 18:26:55 -0500

    initial device type commit

---
+++ devicetypes/melancon/smart-light-ct.src/smart-light-ct.groovy
@@ -0,0 +1,91 @@
+/**
+*  Smart Light - CT
+*
+*  Copyright 2015 Michael Melancon
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*/
+metadata {
+	definition (name: "Smart Light - CT", namespace: "melancon", author: "Michael Melancon") {
+		capability "Actuator"
+		capability "Sensor"
+		capability "Switch"
+		capability "Switch Level"
+
+		command "sync"
+		command "setColorTemperature", ["number"]
+
+		attribute "colorTemperature", "number"
+	}
+
+	standardTile("switch", "device.switch", width:2, height: 2, canChangeIcon: true) {
+		state "on", label:'${name}', action:"switch.off", icon:"st.lights.multi-light-bulb-on", backgroundColor:"#79b821", nextState:"turningOff"
+		state "off", label:'${name}', action:"switch.on", icon:"st.lights.multi-light-bulb-off", backgroundColor:"#ffffff", nextState:"turningOn"
+		state "turningOn", label:'${name}', action:"switch.off", icon:"st.lights.multi-light-bulb-on", backgroundColor:"#79b821", nextState:"turningOff"
+		state "turningOff", label:'${name}', action:"switch.on", icon:"st.lights.multi-light-bulb-off", backgroundColor:"#ffffff", nextState:"turningOn"
+	}
+
+	standardTile("sync", "device.level", decoration: "flat", inactiveLabel: false) {
+		state "default", label:"Sync", action:"sync", icon:"st.secondary.refresh-icon"
+	}
+
+	controlTile("levelControl", "device.level", "slider", height: 1, width: 3, range:"(0..100)") {
+		state "default", action:"switch level.setLevel"
+	}
+
+	controlTile("colorTemperatureControl", "device.colorTemperature", "slider", height: 1, width: 3, range:"(153..500)",
+	backgroundColors:[
+	[value: 150, color: "#bc2323"],
+	[value: 250, color: "#d04e00"],
+	[value: 300, color: "#f1d801"],
+	[value: 325, color: "#44b621"],
+	[value: 350, color: "#90d2a7"],
+	[value: 400, color: "#1e9cbb"],
+	[value: 500, color: "#153591"]
+	]) {
+		state "default", action:"setColorTemperature"
+	}
+
+	main(["switch"])
+
+	details(["switch", "sync", "levelControl", "colorTemperatureControl"])
+}
+
+def parse(description) {
+}
+
+def on() {
+	log.debug "Requesting 'on'"
+	sendEvent(name: "switch", value: "on")
+	parent.on(this)
+}
+
+def off() {
+	log.debug "Requesting 'off'"
+	sendEvent(name: "switch", value: "off")
+	parent.off(this)
+}
+
+def setLevel(percent) {
+	log.debug "Requesting 'setLevel($percent)'"
+	sendEvent(name: "level", value: percent)
+	parent.setLevel(this, percent)
+}
+
+def setColorTemperature(mirek) {
+	log.debug "Requesting 'setColorTemperature($mirek)'"
+	sendEvent(name: "colorTemperature", value: mirek)
+	parent.setColorTemperature(this, mirek)
+}
+
+def sync() {
+	log.debug "Executing 'sync'"
+}
---
+++ devicetypes/melancon/smart-light-fc.src/smart-light-fc.groovy
@@ -0,0 +1,127 @@
+/**
+*  Smart Light - FC
+*
+*  Copyright 2015 Michael Melancon
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*/
+metadata {
+	definition (name: "Smart Light - FC", namespace: "melancon", author: "Michael Melancon") {
+		capability "Actuator"
+		capability "Sensor"
+		capability "Switch"
+		capability "Switch Level"
+		capability "Color Control"
+
+		command "sync"
+		command "resetColor"
+
+		// color temperature control capability
+		command "setColorTemperature", ["number"]
+		attribute "colorTemperature", "number"
+        
+        // color control mode capability
+        command "setColorControlMode", ["string"]
+        attribute "colorControlMode", "enum", ["color control", "color temperature control"]
+	}
+
+	standardTile("switch", "device.switch", width:2, height: 2, canChangeIcon: true) {
+		state "on", label:'${name}', action:"switch.off", icon:"st.lights.multi-light-bulb-on", backgroundColor:"#79b821", nextState:"turningOff"
+		state "off", label:'${name}', action:"switch.on", icon:"st.lights.multi-light-bulb-off", backgroundColor:"#ffffff", nextState:"turningOn"
+		state "turningOn", label:'${name}', action:"switch.off", icon:"st.lights.multi-light-bulb-on", backgroundColor:"#79b821", nextState:"turningOff"
+		state "turningOff", label:'${name}', action:"switch.on", icon:"st.lights.multi-light-bulb-off", backgroundColor:"#ffffff", nextState:"turningOn"
+	}
+
+	standardTile("reset", "device.level", decoration: "flat", inactiveLabel: false) {
+		state "default", label:"Reset Color", action:"reset", icon:"st.illuminance.illuminance.dark"
+	}
+
+	standardTile("sync", "device.level", decoration: "flat", inactiveLabel: false) {
+		state "default", label:"Sync", action:"sync", icon:"st.secondary.refresh-icon"
+	}
+
+	controlTile("levelControl", "device.level", "slider", height: 1, width: 3, range:"(0..100)") {
+		state "default", action:"switch level.setLevel"
+	}
+
+	controlTile("colorTemperatureControl", '${-> device?.currentValue("colorControlMode") == "color control" ? "device.colorTemperature" : "device.color"}', '${-> device?.currentValue("colorControlMode") == "color control" ? "color" : "slider"}', height: 1, width: 3, range:"(153..500)") {
+		state "default", action:"setColorTemperature"
+	}
+
+	controlTile("colorControl", "device.color", "color", height: 3, width: 3) {
+		state "default", action:"color control.setColor"
+	}
+
+	main(["switch"])
+
+	details( ["switch", "sync", "levelControl", "reset", "colorControl", "colorTemperatureControl"])
+
+	preferences()
+}
+
+def parse(description) {
+}
+
+def on() {
+	log.debug "Requesting 'on'"
+	sendEvent(name: "switch", value: "on")
+	parent.on(this)
+}
+
+def off() {
+	log.debug "Requesting 'off'"
+	sendEvent(name: "switch", value: "off")
+	parent.off(this)
+}
+
+def setLevel(percent) {
+	log.debug "Requesting 'setLevel($percent)'"
+	sendEvent(name: "level", value: percent)
+	parent.setLevel(this, percent)
+}
+
+def setColorTemperature(mirek) {
+	log.debug "Requesting 'setColorTemperature($mirek)'"
+	sendEvent(name: "colorTemperature", value: mirek)
+	parent.setColorTemperature(this, mirek)
+}
+
+def setSaturation(percent) {
+	log.debug "Requesting 'setSaturation($percent)'"
+	sendEvent(name: "saturation", value: percent)
+	parent.setSaturation(this, percent)
+}
+
+def setHue(percent) {
+	log.debug "Requesting 'setHue($percent)'"
+	sendEvent(name: "hue", value: percent)
+	parent.setHue(this, percent)
+}
+
+def setColor(color) {
+	color.remove("level")
+	log.debug "Requesting 'setColor($color)'"
+    // this does not seem right it breaks the capability contract, but it's the only way the color controlTile will function properly.
+    // the following line should be: sendEvent(name: "color", value: color)
+	sendEvent(name: "color", value: color.hex)
+    if (color.hue) sendEvent(name: "hue", value: color.hue)
+    if (color.saturation) sendEvent(name: "saturation", value: color.saturation)
+    parent.setColor(this, color)
+}
+
+def resetColor() {
+	log.debug "Executing 'resetColor'"
+	setColor([saturation: 0, hue: 0, hex: '#ffffff'])
+}
+
+def sync() {
+	log.debug "Executing 'sync'"
+}
